c170e8b9d9fdf51623ffc734e51c5ef11819d47b
bruno de sousa behrmann <bruno@DESKTOP-MAF6AMJ>
2024-08-15 23:00:51 -0300

    att

--- SistemaDeBiblioteca/src/lib/Aluno.java
+++ SistemaDeBiblioteca/src/lib/Aluno.java
@@ -1,5 +1,16 @@
 package lib;
 
-public interface Aluno {
+import java.util.List;
 
+public class Aluno extends Usuario {
+	private List<Exemplar> emprestimos;
+	private int qntEmprestimos;
+	
+	
+	public boolean temEmprestimo(Exemplar exemplarVerificar) {
+		for (Exemplar exemplarQPossui: this.emprestimos) {
+			return exemplarQPossui. == exemplarVerificar;
+		}
+		return false;
+	}
 }
