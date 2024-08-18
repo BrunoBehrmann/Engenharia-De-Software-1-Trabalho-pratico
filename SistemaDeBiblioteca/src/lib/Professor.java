f386ce11e9acab18ec991234372169604fb64681
marmeladov69 <115194380+marmeladov69@users.noreply.github.com>
2024-08-18 15:45:10 -0300

    Add files via upload
    Add class Professor (unfinished)

---
+++ SistemaDeBiblioteca/src/lib/Professor.java
@@ -0,0 +1,6 @@
+public class Professor implements Observador {
+  public void atualizar(Usuario usuario) {
+    int qntReservas = ((Usuario) usuario).getQntReservas();
+    System.out.println("Quantidade de Reservas: " + temp);
+  }
+}
