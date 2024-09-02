bc88da06be43207c55b708cfce1f25b51c28f0bb
JB <transgenico0@gmail.com>
2024-09-01 22:23:35 -0300

    modified:   SistemaDeBiblioteca/bin/lib/command/ConsultarVezesNotificaoReservaComando.class 	modified:   SistemaDeBiblioteca/bin/lib/command/ReservarLivroComando.class 	modified:   SistemaDeBiblioteca/bin/lib/users/Professor.class

--- SistemaDeBiblioteca/src/lib/Fabrica.java
+++ SistemaDeBiblioteca/src/lib/Fabrica.java
@@ -1,5 +1,9 @@
 package lib;
 
+import java.time.LocalDateTime;
+
 public class Fabrica {
-    
+    public static Emprestimo criarEmprestimo(Usuario usuario, Exemplar exemplar, LocalDateTime LocalDateTime, int dias) {
+        return new Emprestimo(usuario, exemplar, LocalDateTime, dias);
+    }
 }
--- SistemaDeBiblioteca/src/lib/Sistema.java
+++ SistemaDeBiblioteca/src/lib/Sistema.java
@@ -28,8 +28,8 @@ public class Sistema {
 		comandos.put("res", new ReservarLivroComando()); // ok!
 		comandos.put("obs", new RegistrarProfessorObservadorComando()); // implementar logica
 		comandos.put("liv", new ConsultarLivroComando()); // ok
-		//comandos.put("usu", new ConsultarUsuarioComando()); // ok!
-		//comandos.put("ntf", new ConsultarVezesNotificaoReservaComando()); // falta
+		comandos.put("usu", new ConsultarUsuarioComando()); // ok!
+		comandos.put("ntf", new ConsultarVezesNotificaoReservaComando()); // falta
 		comandos.put("sai", new SairComando()); // ok!
 	}
 
